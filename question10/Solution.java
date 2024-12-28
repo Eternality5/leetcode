2bda98df6b6079eec1e9fea0716cea7c877c89d1
Max Schuller <83692139+Eternality5@users.noreply.github.com>
2024-12-28 07:27:14 +0000

    done some of question 10

--- question10/Solution.java
+++ question10/Solution.java
@@ -1,19 +1,58 @@
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Stack;
+
+import org.w3c.dom.Node;
 
 class Solution {
     public boolean isMatch(String s, String p) {
         // Build FSA
-        HashMap<String, HashSet> fsa = new HashMap<>();
+        Node currentNode = generateFsa(stringToReverseStack(p));
 
-    
         // Traverse FSA
-        HashSet<String> currentNodes = new HashSet<>();
+        
         return false;
     }
 
-    class Node {
-        HashSet<Node> epsilonTransitions;
-        MultiMap<String, Node> transitions;
-        Character character;
+    private Stack<Character> stringToReverseStack(String p) {
+        Stack<Character> stack = new Stack<>();
+        Char[] pattern = p.toCharArray();
+        int length = p.length();
+
+        for (int i = length-1; i >= 0; i--) {
+            stack.push(pattern[i]);
+        }
+        return stack;
     }
+
+    private Node generateFsa(Stack<Character> stack) {
+        char currentChar, nextChar;
+        Node current = new Node(null, null, null, null);
+
+        while(!stack.empty()) {
+            currentChar = stack.pop();
+            if (!stack.empty()) {
+                nextChar = stack.pop();
+
+                if ( (nextChar == '.' && !stack.empty() && stack.peek() == '*') || 
+                     (nextChar != '*')
+                ) {
+                    // put the char back
+                    stack.push(nextChar); 
+                    // just process current char
+                    return new Node(currentChar, generateFsa(stack), null, false); 
+                } else {
+                    return (stack.empty()) 
+                    ? new Node(currentChar, null, null, true)
+                    : new Node(currentChar, null, generateFsa(stack), false);
+                }
+
+            } else {
+                return new Node(currentChar, null, null, true);
+            }
+        }
+    }
+
+    public record Node (Character character, Node transitions, Node epsilonTransitions, boolean isEnd)
+    {}
 }
